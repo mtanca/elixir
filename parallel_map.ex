f048bebf7dbc7ecc3e6373b4add933953ec0db77
Mark Tanca <m.tanca@hotmail.com>
2018-11-25 13:19:20 -0500

    Update parallel_map.ex

--- parallel_map.ex
+++ parallel_map.ex
@@ -1,13 +1,14 @@
 defmodule ParallelExample do
   def pmap(list,function) do
     current_process = self()
+    
     list
     |> Enum.map(fn(element) ->
-      spawn_link fn -> send process, { self(), function.(element) } end end)
+      spawn_link fn -> send current_process, {self(), function.(element)} end end)
     |> Enum.map(fn(pid) ->
       receive do{^pid, result} -> result end end)
   end
 end
 
-# iex(1)> Parallel.pmap [1,2,3,4,5,6], fn(element) -> element * 2 end
+# iex(1)> ParallelExample.pmap [1,2,3,4,5,6], fn(element) -> element * 2 end
 # [2, 4, 6, 8, 10, 12]
